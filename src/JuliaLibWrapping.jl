4df15289b6da56114b10e78199e182737b3d0419
Tim Holy <tim.holy@gmail.com>
2025-07-28 05:24:35 -0500

    Working version of `parselog`

--- src/JuliaLibWrapping.jl
+++ src/JuliaLibWrapping.jl
@@ -1,5 +1,9 @@
 module JuliaLibWrapping
 
-# Write your package code here.
+using Graphs
+
+export parselog
+
+include("parselog.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,42 @@ using JuliaLibWrapping
 using Test
 
 @testset "JuliaLibWrapping.jl" begin
-    # Write your tests here.
+    @testset "parselog" begin
+        entrypoints, typedescs = parselog("bindinginfo_simplelib.log")
+        methdesc = only(entrypoints)
+        @test methdesc.name == "copyto_and_sum"
+        @test methdesc.return_type == "Float32"
+        @test length(methdesc.args) == 1
+        argdesc = only(methdesc.args)
+        @test argdesc.name == "fromto"
+        @test argdesc.type == "CVectorPair{Float32}"
+        @test argdesc.isva == false
+        @test length(typedescs) == 2
+        tdesc = typedescs["CVectorPair{Float32}"]
+        @test tdesc.name == "CVectorPair{Float32}"
+        @test length(tdesc.fields) == 2
+        @test tdesc.fields[1].name == "from"
+        @test tdesc.fields[1].type == "CVector{Float32}"
+        @test tdesc.fields[1].offset == 0
+        @test tdesc.fields[2].name == "to"
+        @test tdesc.fields[2].type == "CVector{Float32}"
+        @test tdesc.fields[2].offset == 16
+        @test tdesc.size == 32
+        tdesc = typedescs["CVector{Float32}"]
+        @test tdesc.name == "CVector{Float32}"
+        @test length(tdesc.fields) == 2
+        @test tdesc.fields[1].name == "length"
+        @test tdesc.fields[1].type == "Int32"
+        @test tdesc.fields[1].offset == 0
+        @test tdesc.fields[2].name == "data"
+        @test tdesc.fields[2].type == "Ptr{Float32}"
+        @test tdesc.fields[2].offset == 8
+        @test tdesc.size == 16
+        @test haskey(typedescs, "CVectorPair{Float32}")
+        @test haskey(typedescs, "CVector{Float32}")
+        @test sprint(show, only(entrypoints)) == "copyto_and_sum(fromto::CVectorPair{Float32})::Float32"
+        str = sprint(show, typedescs)
+        @test occursin("CVectorPair{Float32}(from::CVector{Float32}[0], to::CVector{Float32}[16]) (32 bytes)", str)
+        @test occursin("CVector{Float32}(length::Int32[0], data::Ptr{Float32}[8]) (16 bytes)", str)
+    end
 end
